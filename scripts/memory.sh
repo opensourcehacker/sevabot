8cf81ef7ef1532ffb2666c3a07dac7f0fae4919d
Pete Sevander <pete.sevander@gmail.com>
2012-07-30 14:42:40 +0300

    changed memory.sh to just print the biggest memory usage

--- scripts/memory.sh
+++ scripts/memory.sh
@@ -1,11 +1,3 @@
 #!/bin/sh
 
-highest=`ps -eo vsize,comm --sort vsize | tail -n 1`
-
-set -- $highest
-
-mem=$1
-
-if [ "$mem" -gt "1228800" ]; then
-    echo $highest
-fi
+ps -eo vsize --sort vsize | tail -n 1
