0de044b8bade5f2b572590f602122f6d490501ed
Pete Sevander <pete.sevander@gmail.com>
2012-07-31 11:44:57 +0300

    updated memoryhog to use ssh config for hosts if '*' is given as an argument

--- modules/memoryhog.sh
+++ modules/memoryhog.sh
@@ -1,3 +1,19 @@
 #!/bin/sh
 
-ssh $1 "ps -eo vsize,args --sort vsize | tail -n 1"
+# I's best to use ssh-agent and .ssh/config for this.
+
+HOST=$1
+
+if [ -z "$HOST" ]; then
+    echo "Usage: !memoryhog <host or *>"
+elif [ "$HOST" = "*" ]; then
+    HOSTS=`grep "Host " ~/.ssh/config | awk '{print $2}'`
+
+    for h in $HOSTS; do
+        echo -n "$h: "
+        ssh $h "ps -eo vsize,args --sort vsize | tail -n 1"
+    done
+
+else
+    ssh $HOST "ps -eo vsize,args --sort vsize | tail -n 1"
+fi
