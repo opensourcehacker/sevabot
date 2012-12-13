0394f74c22efac311b123f32eb8b4ae9ccfb677e
Grigory Chernyshev <g.chernyshev@corp.mail.ru>
2012-12-13 19:18:27 +0400

    remove unused imports

--- sevabot/bot/bot.py
+++ sevabot/bot/bot.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-from glob import glob
-import imp
 import sys
 import logging
 import hashlib
